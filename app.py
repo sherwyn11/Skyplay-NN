f25f43b3d76c1b06c0fbf91702c29b2ceea19845
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-05-14 21:18:45 +0530

    boilerplate code and added activation functions

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify, render_template
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0')
