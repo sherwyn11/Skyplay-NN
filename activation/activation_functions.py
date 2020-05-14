fe91b0e293ad6053dcff6c707b74997f4cb8ff40
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-05-14 21:22:48 +0530

    changes made

--- activation/activation_functions.py
+++ activation/activation_functions.py
@@ -17,13 +17,13 @@ def leakyrelu(data, alpha):
 ######## Derivative of Activation (Backward Propagation) ########
 
 def sigmoid_diff(data):
-  return sigmoid(data) * (1-sigmoid(data))
+    return sigmoid(data) * (1-sigmoid(data))
 
 def tanh_diff(data):
 	return 1 - np.power(tanh(data), 2)
 
 def relu_diff(data):
-  return 1 if data > 0 else 0
+    return 1 if data > 0 else 0
 
 def leakyrelu_diff(data, alpha):
 	return 1 if data > 0 else alpha
