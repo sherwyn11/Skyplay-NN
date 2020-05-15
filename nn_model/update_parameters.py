95a5096243c4d314a89cf1558acb0f0532cc36b8
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-05-16 00:17:19 +0530

    error

---
+++ nn_model/update_parameters.py
@@ -0,0 +1,9 @@
+def update_parameters(parameters, grads, learning_rate):
+  
+    L = len(parameters)
+
+    for l in range(L):
+        parameters["W" + str(l+1)] = parameters["W" + str(l+1)]  - learning_rate * grads["dW" + str(l+1)]
+        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate * grads["db" + str(l+1)] 
+   
+    return parameters
