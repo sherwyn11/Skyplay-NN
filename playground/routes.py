b8f4772731ff9b61d0a71a41caa92c1838213513
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-05-21 00:12:57 +0530

    preprocess and visualize linked

--- playground/routes.py
+++ playground/routes.py
@@ -1,25 +1,27 @@
 from flask import *
 import numpy as np
+
 from playground import app
 from playground.neural_net.nn_model.model import Model
 from playground.preprocessing import generic_preprocessing as gp
 from playground.nocache import nocache
 from playground.utils.feature_extract import get_features
 from playground.utils.flask_utils import *
-from playground.visualization.visualize import *
+from playground.visualization import visualize as vis
 
 
 global posted
 model = Model()
-save_path = 'weka/uploads/'
+save_path = 'playground/uploads/'
 posted = 0
 
-# --------------------- ROUTES ---------------------------
+####################### ROUTES #######################
 
 
 @nocache
 @app.route('/', methods=['GET', 'POST'])
 def home():
+
     global model
     if request.method == 'POST':
         if request.get_json() is not None:
@@ -51,8 +53,7 @@ def home():
                 flash(f'Upload Unsuccessful. Please try again', 'danger')
 
         elif request.form['Submit'] == 'preprocess':
-            print('True')
-            return 'True'
+            return redirect('/preprocess')
 
     else:
         if session.get('uploaded') is None and session.get('posted') is None:
@@ -96,6 +97,132 @@ def home():
     )
 
 
+@app.route('/preprocess', methods=['GET', 'POST'])
+@nocache
+def preprocess():
+
+    if request.method == 'POST':
+
+        if request.form['Submit'] == 'Delete':
+            try:
+                df = gp.read_dataset('playground/clean/clean.csv')
+                df = gp.delete_column(df, request.form.getlist('check_cols'))
+                df.to_csv('playground/clean/clean.csv', mode='w', index=False)
+                flash(f'Column(s) deleted Successfully', 'success')
+            except:
+                flash(f'Error! Upload Dataset', 'danger')
+
+        elif request.form['Submit'] == 'Clean':
+            try:
+                df = gp.read_dataset('playground/clean/clean.csv')
+                print(request.form['how'])
+                if request.form['how'] is not 'any':
+                    df = gp.treat_missing_numeric(
+                        df, request.form.getlist('check_cols'), how=request.form['how']
+                    )
+                elif request.form['howNos'] is not None:
+                    df = gp.treat_missing_numeric(
+                        df,
+                        request.form.getlist('check_cols'),
+                        how=float(request.form['howNos']),
+                    )
+
+                df.to_csv('playground/clean/clean.csv', mode='w', index=False)
+                flash(f'Column(s) cleant Successfully', 'success')
+            except:
+                flash(f'Error! Upload Dataset', 'danger')
+
+        elif request.form['Submit'] == 'Visualize':
+            global posted
+            df = gp.read_dataset('playground/clean/clean.csv')
+
+            x_col = request.form['x_col']
+
+            if vis.hist_plot(df, x_col):
+                posted = 1
+
+    df = gp.read_dataset('playground/clean/clean.csv')
+    description = gp.get_description(df)
+    columns = gp.get_columns(df)
+    print(columns)
+    dim1, dim2 = gp.get_dim(df)
+    head = gp.get_head(df)
+
+    return render_template(
+        'preprocess.html',
+        active='preprocess',
+        title='Preprocess',
+        filename=session['fname'],
+        posted=posted,
+        no_of_rows=len(df),
+        no_of_cols=len(columns),
+        dim=str(dim1) + ' x ' + str(dim2),
+        description=description.to_html(
+            classes=[
+                'table-bordered',
+                'table-striped',
+                'table-hover',
+                'thead-light',
+            ]
+        ),
+        columns=columns,
+        head=head.to_html(
+            classes=[
+                'table',
+                'table-bordered',
+                'table-striped',
+                'table-hover',
+                'thead-light',
+            ]
+        ),
+    )
+
+
+@app.route('/visualize', methods=['GET', 'POST'])
+@nocache
+def visualize():
+
+    if request.method == 'POST':
+        x_col = request.form['x_col']
+        y_col = request.form['y_col']
+
+        df = vis.xy_plot(x_col, y_col)
+        heights = np.array(df[x_col]).tolist()
+        weights = np.array(df[y_col]).tolist()
+
+        newlist = []
+        for h, w in zip(heights, weights):
+            newlist.append({'x': h, 'y': w})
+        ugly_blob = str(newlist).replace("'", '')
+        print(newlist)
+        columns = vis.get_columns()
+        print(x_col)
+        return render_template(
+            'visualize.html',
+            cols=columns,
+            src='img/pairplot.png',
+            xy_src='img/fig.png',
+            posted=1,
+            data=ugly_blob,
+            active='visualize',
+            x_col_name=str(x_col),
+            y_col_name=str(y_col),
+            title='Visualize',
+        )
+
+    else:
+        vis.pair_plot()
+        columns = vis.get_columns()
+        return render_template(
+            'visualize.html',
+            cols=columns,
+            src='img/pairplot.png',
+            posted=0,
+            active='visualize',
+            title='Visualize',
+        )
+
+
 @nocache
 @app.route('/data.json', methods=['GET'])
 def data():
@@ -105,7 +232,7 @@ def data():
     data = {'nodes': [], 'weights': []}
     SUB = str.maketrans('0123456789', '₀₁₂₃₄₅₆₇₈₉')
     SUP = str.maketrans('0123456789', '⁰¹²³⁴⁵⁶⁷⁸⁹')
-    lst = get_columns()
+    lst = vis.get_columns()
 
     for i in range(len(lst) - 1):
         data['nodes'].append({'label': lst[i], 'layer': 1})
@@ -137,5 +264,22 @@ def test():
 def reset():
     session.clear()
     default_data()
-    session['posted'] = 0
+    session['posted'] = None
     return redirect('/')
+
+@app.route('/col.csv')
+@nocache
+def col():
+    return send_file('visualization/col.csv', mimetype='text/csv', as_attachment=True)
+
+
+# @app.route('/pairplot.png')
+# @nocache
+# def pairplot():
+#     return send_file(
+#         'static/img/pairplot.png', mimetype='image/png', as_attachment=True
+#     )
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
