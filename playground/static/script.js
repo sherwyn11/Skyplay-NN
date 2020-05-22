79b07b31878bb0f8e747417a02b8cad4de6cdab1
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-05-22 22:55:34 +0530

    made some changes

--- playground/static/script.js
+++ playground/static/script.js
@@ -367,3 +367,192 @@ d3.json("data.json", function(error, graph) {
             return d.label;
         });
 });
+
+var margin = {
+    top: 20,
+    right: 30,
+    bottom: 30,
+    left: 40
+},
+width = 600 - margin.left - margin.right,
+height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+.append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+
+// A formatter for counts.
+var formatCount = d3.format(",.0f");
+
+// get the data
+d3.csv("/col.csv", function(data) {
+
+    var yAxis
+
+    // A function that builds the graph for a specific value of bin
+    function update(nBin) {
+
+        // X axis: scale and draw:
+        var x = d3.scaleLinear()
+            .domain([d3.min(data, function(d) {
+                return +d.price
+            }), d3.max(data, function(d) {
+                return +d.price
+            })]) // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+            .range([0, width]);
+
+        var xAxis = svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+
+        // set the parameters for the histogram
+        var histogram = d3.histogram()
+            .value(function(d) {
+                return d.price;
+            }) // I need to give the vector of value
+            .domain(x.domain()) // then the domain of the graphic
+            .thresholds(x.ticks(nBin)); // then the numbers of bins
+
+        // And apply this function to data to get the bins
+        var bins = histogram(data);
+
+        // Y axis: initialization
+        var y = d3.scaleLinear()
+            .range([height, 0]);
+
+        yAxis = svg.append("g")
+
+        // Y axis: update now that we know the domain
+        y.domain([0, d3.max(bins, function(d) {
+            return d.length;
+        })]); // d3.hist has to be called before the Y axis obviously
+
+        yAxis
+            .enter()
+            .merge(yAxis)
+            .transition()
+            .duration(1000)
+            .call(d3.axisLeft(y));
+
+
+
+        // Join the rect with the bins data
+        var u = svg.selectAll(".bar")
+            .data(bins)
+
+        ///////////// Method 1 : Color Works
+        //Append a defs (for definition) element to your SVG
+        var defs = svg.append("defs");
+
+        //Append a linearGradient element to the defs and give it a unique id
+        var linearGradient = defs.append("linearGradient");
+
+        //Horizontal gradient
+        linearGradient
+            .attr("x1", "100%")
+            .attr("y1", "100%")
+            .attr("x2", "0%")
+            .attr("y2", "0%")
+            .attr("id", "linear-gradient");
+
+        //Set the color for the start (0%)
+        linearGradient.append("stop")
+            .attr("offset", "0%")
+            .attr("stop-color", "#ffa474"); //light blue
+
+        //Set the color for the end (100%)
+        linearGradient.append("stop")
+            .attr("offset", "100%")
+            .attr("stop-color", "#8b0000"); //dark blue
+
+        ///////////// Method 2 : X
+        color = d3.scaleSequential()
+            .domain([0, d3.max(data, d => d.price)])
+            .interpolator(d3.interpolateBlues)
+
+        ///////////// Method 3 : Works
+
+        var color = "steelblue";
+        var color2 = "purple"
+        var yMax = d3.max(data, function(d) {
+            return d.price
+        });
+        var yMin = d3.min(data, function(d) {
+            return d.price
+        });
+        var colorScale = d3.scaleLinear()
+            .domain([0, yMax])
+            .range([d3.rgb(color).brighter(), d3.rgb(color)]);
+
+
+
+        // Manage the existing bars and eventually the new ones:
+        u
+            .enter()
+            .append("rect") // Add a new rect for each new elements
+            .merge(u) // get the already existing elements as well
+            .transition() // and apply changes to all of them
+            .duration(1000)
+            .attr("x", 1)
+            .attr("transform", function(d) {
+                return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+            })
+            .attr("width", function(d) {
+                return x(d.x1) - x(d.x0) - 1;
+            })
+            .attr("height", function(d) {
+                return height - y(d.length);
+            })
+            .style("fill", function(d) {
+                return colorScale(d.length)
+            })
+
+
+        u.enter()
+            .append("text")
+            .merge(u)
+            .transition()
+            .duration(1000)
+            .attr("dy", ".6em")
+            .attr("y", function(d) {
+                return (y(d.length) - 13);
+            })
+            .attr("x", function(d) {
+                return (x(d.x1) - (x(d.x1) - x(d.x0)) / 2)
+            })
+            .attr("text-anchor", "middle")
+            .attr("class", "font-weight-light")
+            .text(function(d) {
+                return formatCount(d.length);
+            })
+
+        // If less bar in the new histogram, I delete the ones not in use anymore
+        u
+            .exit()
+            .remove()
+
+
+
+    }
+
+
+    // Initialize with 20 bins
+    update(20)
+
+
+    // Listen to the button -> update if user change it
+    d3.select("#nBin").on("input", function() {
+        d3.selectAll('text').remove()
+        d3.selectAll('rect').remove()
+        yAxis.remove()
+        update(+this.value);
+    });
+
+});
